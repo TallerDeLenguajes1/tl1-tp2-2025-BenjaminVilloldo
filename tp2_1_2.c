ca4cb6c874d3917edfc346b845b994f9edfdfbad
Benjamin Villoldo <benjaminvillol@gmail.com>
2025-04-04 11:56:01 -0300

    Copiado el archivo en la segunda version

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-// codigo a completar
+#include <stdlib.h>  // Para rand()
+#include <time.h>    // Para srand()
+
 #define N 20
-int i;
-double vt[N];
-for(i = 0;i<N; i++)
-{
-vt[i]=1+rand()%100;
-printf(“%f ”, vt[i]);
+
+int main() {
+    double vt[N];
+    double *ptr = vt;  // Puntero que apunta al inicio del array
+
+    // Inicializar la semilla del generador de números aleatorios
+    srand(time(NULL));
+
+    // Usando aritmética de punteros para recorrer el array
+    for (int i = 0; i < N; i++) {
+        *(ptr + i) = 1 + rand() % 100;  // Asignar valor usando puntero
+        printf("%f ", *(ptr + i));      // Imprimir valor usando puntero
+    }
+
+    printf("\n");
+    return 0;
 }
